e39cc3336fa45c8bf94ce0378425929ab08ddf8b
Luis Sousa <las.sousa@outlook.com>
2023-11-05 15:10:52 -0300

    add anotacoes lombok

--- src/main/java/com/luis/trabalhoPweb/entities/Medico.java
+++ src/main/java/com/luis/trabalhoPweb/entities/Medico.java
@@ -2,9 +2,16 @@ package com.luis.trabalhoPweb.entities;
 
 import com.luis.trabalhoPweb.dtos.MedicoDTO;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Cascade;
 
 import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 @Entity
 @Table(name = "tb_medico")
@@ -23,19 +30,6 @@ public class Medico {
     @Cascade(org.hibernate.annotations.CascadeType.ALL)
     private Endereco endereco;
     private Boolean ativo;
-    public Medico() {
-    }
-
-    public Medico(Long id, String nome, String email, String telefone, String crm, Especialidade especialidade, Endereco endereco, Boolean ativo) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.telefone = telefone;
-        this.crm = crm;
-        this.especialidade = especialidade;
-        this.endereco = endereco;
-        this.ativo = ativo;
-    }
 
     public Medico(MedicoDTO medicoDTO) {
         this.nome = medicoDTO.getNome();
@@ -45,82 +39,5 @@ public class Medico {
         this.especialidade = medicoDTO.getEspecialidade();
         this.endereco = medicoDTO.getEndereco();
         this.ativo = medicoDTO.getAtivo();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Medico medico = (Medico) o;
-        return Objects.equals(id, medico.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getCrm() {
-        return crm;
-    }
-
-    public void setCrm(String crm) {
-        this.crm = crm;
-    }
-
-    public Especialidade getEspecialidade() {
-        return especialidade;
-    }
-
-    public void setEspecialidade(Especialidade especialidade) {
-        this.especialidade = especialidade;
-    }
-
-    public Endereco getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
-
-    public Boolean getAtivo() {
-        return ativo;
-    }
-
-    public void setAtivo(Boolean ativo) {
-        this.ativo = ativo;
     }
 }
